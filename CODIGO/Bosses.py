76b947e8ebfe6214780286742d50fed0a1184cd5
Jcontrerasm <126794784+PachitoBott@users.noreply.github.com>
2025-11-22 21:10:11 -0500

    Refine projectile visuals and scale up boss shots

--- CODIGO/Bosses.py
+++ CODIGO/Bosses.py
@@ -701,8 +701,9 @@ class CorruptedServerBoss(BossEnemy):
                 dx,
                 dy,
                 speed=speed,
-                radius=4,
+                radius=6,
                 color=(180, 220, 255),
+                is_boss_projectile=True,
             )
             out_bullets.add(proj)
 
@@ -724,8 +725,9 @@ class CorruptedServerBoss(BossEnemy):
                 dx,
                 dy,
                 speed=speed,
-                radius=4,
+                radius=6,
                 color=(255, 160, 80),
+                is_boss_projectile=True,
             )
             out_bullets.add(proj)
 
@@ -778,8 +780,9 @@ class CorruptedServerBoss(BossEnemy):
                 dx,
                 dy,
                 speed=320.0,
-                radius=3,
+                radius=5,
                 color=(255, 255, 120),
+                is_boss_projectile=True,
             )
             out_bullets.add(proj)
 
